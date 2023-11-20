cf73fc1d644f7bf08ae79bdaa0435f844b04289e
Catia Fernandez <katia.fernandezmacias@gmail.com>
2023-11-20 21:59:15 +0100

    cambio de tematica y ultimos ajutes: formulario añadido de url en vez de seleccion de foto

--- src/app/db/posts.db.ts
+++ src/app/db/posts.db.ts
@@ -4,71 +4,39 @@ import { IPost } from "../interfaces/post.interface";
 export const POSTS: IPost[] = [
     {
         id: 1,
-        titulo: 'Mi Experiencia en el Bootcamp de Programación',
-        texto: 'Durante los últimos tres meses, participé en un bootcamp intensivo de programación que transformó por completo mi perspectiva y habilidades en el mundo del desarrollo de software. Aprendí desde los conceptos básicos hasta proyectos avanzados, lo que me preparó para enfrentar desafíos del mundo real.',
-        autor: 'Ana Pérez',
-        imagen: 'https://t3.ftcdn.net/jpg/02/77/13/10/360_F_277131011_MbhF7Di6LxKJq0FGAg5Z3958RUsegRPt.jpg',
+        titulo: ' Aventura en las Alturas',
+        texto: 'Volando entre las nubes: Mi experiencia en paracaidismo. ¡La adrenalina estaba en su punto máximo! Mi corazón latía con fuerza mientras saltaba de un avión a miles de metros de altura. En este post, comparto mi fascinante experiencia con el paracaidismo y cómo esa caída libre me hizo sentir vivo como nunca antes. La sensación de libertad, la vista panorámica y la emoción inigualable, ¡todo en un solo salto!',
+        autor: 'AventureroXtreme',
+        imagen: 'https://images.unsplash.com/photo-1664494130837-14e0473ed284?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzV8fHBhcmFjYWlkaXNtb3xlbnwwfDB8MHx8fDA%3D',
         fecha: '2023-11-30',
-        categoria: 'Desarrollo de Software'
+        categoria: 'Aventuras Extremas'
     },
     {
         id: 2,
-        titulo: 'Cómo Aprendí Desarrollo Web en el Bootcamp',
-        texto: 'Exploro mi viaje desde no saber nada sobre desarrollo web hasta construir aplicaciones completas utilizando las tecnologías más relevantes. ¡El bootcamp fue una montaña rusa de aprendizaje!',
-        autor: 'Juan Martínez',
-        imagen: 'https://img.freepik.com/fotos-premium/palmeras-playa-al-atardecer_865967-45544.jpg',
+        titulo: 'Descubriendo la Magia Submarina',
+        texto: 'Buceo en las profundidades: Un encuentro con la vida marina. Sumergirse en las aguas cristalinas y explorar el fascinante mundo submarino fue una experiencia inolvidable. Descubrí arrecifes de colores, nadé con peces tropicales y presencié la majestuosidad de las criaturas marinas. Este post detalla mi emocionante jornada de buceo y cómo la conexión con la naturaleza acuática dejó una marca duradera en mi corazón.',
+        autor: 'ExploradorAcuático',
+        imagen: 'https://images.unsplash.com/photo-1518467166778-b88f373ffec7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fHN1Ym1hcmluaXNtb3xlbnwwfDB8MHx8fDA%3D',
         fecha: '2023-11-28',
-        categoria: 'Desarrollo Web'
+        categoria: 'Aventuras Acuáticas'
     },
     {
         id: 3,
-        titulo: 'Desafíos y Éxitos: Mi Bootcamp en el Mundo de la Inteligencia Artificial',
-        texto: 'Comparto los desafíos emocionantes y los éxitos que experimenté mientras exploraba la inteligencia artificial en el bootcamp. Desde algoritmos hasta redes neuronales, cada día fue una nueva aventura.',
-        autor: 'Laura Soto',
-        imagen: 'https://images.unsplash.com/photo-1506929562872-bb421503ef21?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjk1fHxiZWF1dGlmdWwlMjBiZWFjaCUyMCUyMHNlYSUyMGpwZ3xlbnwwfHwwfHx8MA%3D%3D',
+        titulo: 'Travesía Cultural por Tierras Exóticas',
+        texto: 'Explorando la esencia de Marruecos: Una travesía cultural. Viajar a Marruecos fue sumergirse en un mundo de colores, aromas y tradiciones únicas. Desde los zocos bulliciosos hasta las dunas del desierto, este post relata mi experiencia inolvidable explorando la rica cultura marroquí. Descubre cómo cada rincón contaba una historia diferente y cómo las amables personas locales hicieron que este viaje fuera aún más especial.',
+        autor: 'ViajeroCultural',
+        imagen: 'https://images.unsplash.com/photo-1559586616-361e18714958?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG1hcnJ1ZWNvc3xlbnwwfDB8MHx8fDA%3D',
         fecha: '2023-11-25',
-        categoria: 'Inteligencia Artificial'
+        categoria: 'Viajes Culturales'
     },
     {
         id: 4,
-        titulo: 'Del Bootcamp a la Práctica: Construyendo Aplicaciones del Mundo Real',
-        texto: 'Discuto cómo el bootcamp no solo me enseñó teoría, sino también cómo aplicar esos conocimientos en proyectos prácticos del mundo real. ¡La experiencia de construir aplicaciones desde cero fue inigualable!',
-        autor: 'Carlos González',
-        imagen: 'https://images.unsplash.com/photo-1449846859890-2e19a995bcfd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjk4fHxiZWF1dGlmdWwlMjBiZWFjaCUyMCUyMHNlYSUyMGpwZ3xlbnwwfHwwfHx8MA%3D%3D',
+        titulo: 'Amanecer en lo Más Alto de las Montañas',
+        texto: 'En la cima del mundo: Mi ascenso al pico más alto. Desafié mis límites y conquisté la montaña más alta de la región. Desde la oscuridad de la madrugada hasta la gloriosa luz del amanecer, este post narra mi épica travesía hacia la cima. Experimenté la serenidad de la naturaleza, la resistencia personal y la recompensa inigualable de alcanzar nuevas alturas. ¡Una historia inspiradora para aquellos que buscan desafíos!',
+        autor: 'MontañistaIntrépido',
+        imagen: 'https://images.unsplash.com/photo-1474418397713-7ede21d49118?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fHBhcmFjYWlkaXNtbyUyMHZpZXd8ZW58MHwwfDB8fHww',
         fecha: '2023-11-22',
-        categoria: 'Desarrollo Práctico'
+        categoria: 'Aventuras en la Naturaleza'
     },
-    // {
-    //     titulo: 'Descubriendo mi Pasión por la Seguridad Informática en el Bootcamp',
-    //     texto: 'Hablo sobre cómo el bootcamp me llevó a descubrir y apasionarme por la seguridad informática. Desde pruebas de penetración hasta análisis forense, cada módulo fue fascinante y desafiante.',
-    //     autor: 'María Rodríguez',
-    //     imagen: 'https://images.unsplash.com/photo-1533787896899-91b040188f57?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzEzfHxiZWF1dGlmdWwlMjBiZWFjaCUyMCUyMHNlYSUyMGpwZ3xlbnwwfHwwfHx8MA%3D%3DI',
-    //     fecha: '2023-11-20',
-    //     categoria: 'Seguridad Informática'
-    // },
-    // {
-    //     titulo: 'Bootcamp de Desarrollo Full Stack: Una Inmersión Total',
-    //     texto: 'Detallo mi experiencia en un bootcamp de desarrollo full stack, abarcando tanto el frontend como el backend. Desde la interfaz de usuario hasta la base de datos, el bootcamp proporcionó una comprensión integral.',
-    //     autor: 'Javier López',
-    //     imagen: 'URL_IMAGEN_5',
-    //     fecha: '2023-11-18',
-    //     categoria: 'Desarrollo Full Stack'
-    // },
-    // {
-    //     titulo: 'Aventuras en el Bootcamp: Creando mi Portafolio y Buscando Empleo',
-    //     texto: 'Comparto cómo el bootcamp no solo me dio habilidades técnicas, sino también las herramientas necesarias para construir un portafolio impresionante y enfrentarme al proceso de búsqueda de empleo.',
-    //     autor: 'Ana Pérez',
-    //     imagen: 'URL_IMAGEN_6',
-    //     fecha: '2023-11-15',
-    //     categoria: 'Carrera Profesional'
-    // },
-    // {
-    //     titulo: 'Bootcamp de Ciencia de Datos: De los Datos a las Decisiones',
-    //     texto: 'Mi experiencia inmersiva en un bootcamp de ciencia de datos, explorando desde la recopilación y limpieza de datos hasta la implementación de modelos predictivos. ¡La ciencia de datos se volvió accesible y emocionante!',
-    //     autor: 'Luis Serrano',
-    //     imagen: 'https://images.unsplash.com/photo-1532189406528-86316fa63c4c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzg1fHxiZWF1dGlmdWwlMjBiZWFjaCUyMCUyMHNlYSUyMGpwZ3xlbnwwfHwwfHx8MA%3D%3D',
-    //     fecha: '2023-11-12',
-    //     categoria: 'Ciencia de Datos'
-    // }
 
 ]
